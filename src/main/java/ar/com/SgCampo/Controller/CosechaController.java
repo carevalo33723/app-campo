f5eaab136d1f4adf73785d0ec3889dd0cda11bb5
cesar arevalo <cesar3.arevalo@gmail.com>
2025-01-13 17:12:06 -0300

    creacion de html cosecha y controlador altaCosecha

--- src/main/java/ar/com/SgCampo/Controller/CosechaController.java
+++ src/main/java/ar/com/SgCampo/Controller/CosechaController.java
@@ -1,8 +1,44 @@
 package ar.com.SgCampo.Controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.com.SgCampo.Model.Entity.Arrendatario;
+import ar.com.SgCampo.Model.Entity.Cosecha;
+import ar.com.SgCampo.Model.Entity.Producto;
+import ar.com.SgCampo.Model.Entity.Socio;
+import ar.com.SgCampo.Model.Serivice.IArrendatarioService;
+import ar.com.SgCampo.Model.Serivice.IProductoService;
+import ar.com.SgCampo.Model.Serivice.ISociosService;
 
 @Controller
 public class CosechaController {
 
+	@Autowired
+	IArrendatarioService dat;
+	@Autowired
+	ISociosService socio;
+	@Autowired
+	IProductoService prod;
+	
+	@GetMapping("/altaCosecha")
+	public String crear(Model model) {
+		//crear la lista para pasar y pueda asociar
+		List <Socio> listaSocio = socio.listar();
+		
+		List <Producto> listaProducto = prod.listar();
+		
+		model.addAttribute("listaSocios",listaSocio);
+		model.addAttribute("listaProductos",listaProducto);
+		
+		Cosecha NuevaCosecha=new Cosecha();
+		
+		model.addAttribute("cosecha", NuevaCosecha);
+		return null;
+	}
 }
